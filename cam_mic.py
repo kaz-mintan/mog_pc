3f24634b12185d2ff850ade759f5a5de16a9f191
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2019-10-08 03:29:01 +0900

    merged mic and cam

---
+++ cam_mic.py
@@ -0,0 +1,12 @@
+import time
+import threading
+
+from rec import MakeWavFile
+from simple_cap import capture
+
+thread_1 = threading.Thread(target=MakeWavFile)
+thread_2 = threading.Thread(target=capture)
+
+
+thread_1.start()
+thread_2.start()
