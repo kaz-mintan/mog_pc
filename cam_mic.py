20210dac87301f1dd5a2b1f2b945568284dedb2b
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2019-10-22 20:59:23 +0900

    add call ard

--- cam_mic.py
+++ cam_mic.py
@@ -3,12 +3,14 @@ import threading
 
 from rec import MakeWavFile
 from simple_cap import capture
+from call_ard import get_hearts
 
 thread_1 = threading.Thread(target=MakeWavFile)
 thread_2 = threading.Thread(target=capture)
-
+thread_3 = threading.Thread(target=get_hearts)
 
 thread_1.start()
 thread_2.start()
+thread_3.start()
 
 thread_1.join()
