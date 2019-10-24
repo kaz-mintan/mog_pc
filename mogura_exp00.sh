834ebdc62705a8441d959d42d14947ccd9a9f1a4
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2019-10-24 23:22:55 +0900

    add new exp dir

--- mogura_exp00.sh
+++ mogura_exp00.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-DIR="/home/kazumi/mogura/pic/"
+DIR="/home/kazumi/mogura/new_exp/"
 if [ ! -e $DIR ];then
   mkdir $DIR
   chmod a+xwr $DIR
 fi
+
+
+DIR2="/home/kazumi/mogura/pic/"
+if [ ! -e $DIR2 ];then
+  mkdir $DIR2
+  chmod a+xwr $DIR2
+fi
+
 echo "カメラ確認。顔の位置を確認してください。Ctrl-Cで終了。"
 python camera_test.py
