0c26018b8f88543df9905a6e88e93a7ddb94d05f
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2019-10-24 21:27:08 +0900

    no argument

--- cam_mic.py
+++ cam_mic.py
@@ -9,8 +9,10 @@ thread_1 = threading.Thread(target=MakeWavFile)
 thread_2 = threading.Thread(target=capture)
 thread_3 = threading.Thread(target=get_hearts)
 
-thread_1.start()
 thread_2.start()
+thread_1.start()
 thread_3.start()
 
 thread_1.join()
+thread_2.join()
+thread_3.join()
