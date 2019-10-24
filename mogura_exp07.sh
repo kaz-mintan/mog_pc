c56bac4f486495525218295edfd2b42ac7f82f7c
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2019-10-24 23:24:14 +0900

    add dir move sh

---
+++ mogura_exp07.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+MOGUDIR=/home/kazumi/mogura
+if [ $# -ne 1 ]; then
+  echo "指定された引数は$#個です。" 1>&2
+  echo "実行するには1個の引数が必要です。" 1>&2
+  exit 1
+fi
+
+DIR=/home/kazumi/mogura/$1
+mkdir $DIR
+chmod a+xwr $DIR
+
+mv $MOGUDIR/sample.wav $DIR
+mv $MOGUDIR/pic $DIR
+mv $MOGUDIR/heart_out.csv $DIR
+mv $MOGUDIR/new_exp $DIR
+
