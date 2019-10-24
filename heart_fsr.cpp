c8838007a81755ada73bf0a21a42dbf68e1ba521
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2019-10-24 19:19:31 +0900

    changed time stump

--- heart_fsr.cpp
+++ heart_fsr.cpp
@@ -69,7 +69,7 @@ int serial_ardinoread(char *devicename,char *messege)
 //	strcpy(mes,"");
 
   FILE *heartfile;
-  heartfile = fopen("out.csv","w");
+  heartfile = fopen("heart_out.csv","w");
 
   if(heartfile==NULL){
     printf("cannot open file\n");
@@ -97,8 +97,8 @@ int serial_ardinoread(char *devicename,char *messege)
           heart = short_buf_b[0]<<8|(short_buf_b[1]&0x00ff);
           //meron = (short_buf[0]<<8&0x00ff)|(short_buf[1]&0x00ff);
           //8bit shift && high 8bit mask/
-          fprintf(heartfile,"%d,%d,%d,%d,%d\n",pressure,heart,(int)t_st->tm_min,(int)t_st->tm_sec,(int)nowTime.tv_usec);
-          printf("%d,%d,%d\n",pressure,heart);
+          fprintf(heartfile,"%d,%d,%d%d-%d:%d:%d.%d\n",pressure,heart,(int)t_st->tm_mon,(int)t_st->tm_mday,(int)t_st->tm_hour,(int)t_st->tm_min,(int)t_st->tm_sec,(int)nowTime.tv_usec);
+          //printf("%d,%d\n",pressure,heart);
   //				val=meron+ringo;
         }
     }
